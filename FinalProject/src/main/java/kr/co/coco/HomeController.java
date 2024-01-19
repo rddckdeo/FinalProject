175c55bfeefd7dd2366195d9b14b8ae4a8d3a946
rddckdeo <rddckdeo@naver.com>
2024-01-19 15:37:56 +0900

    11

--- FinalProject/src/main/java/kr/co/coco/HomeController.java
+++ FinalProject/src/main/java/kr/co/coco/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		
+		// test
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
