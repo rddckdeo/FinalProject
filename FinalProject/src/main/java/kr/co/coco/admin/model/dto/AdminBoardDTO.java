a0576a148def0a65e3881079d5cedad0cf08f405
rddckdeo <rddckdeo@naver.com>
2024-02-16 17:46:26 +0900

    commit

---
+++ FinalProject/src/main/java/kr/co/coco/admin/model/dto/AdminBoardDTO.java
@@ -0,0 +1,22 @@
+package kr.co.coco.admin.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminBoardDTO {
+	private int no;
+	private String title;
+	private String content;
+	private int view;
+	private String inDate;
+	private String updateDate;
+	private String deleteDate;
+	private String answer;
+	private String answerContent;
+	private String answerDate;
+	
+	private int mNo;
+	private String name;
+}
